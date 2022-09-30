30653c9e2f46f1f71b8a3b9fdcc6d573da72e559
Ihor Ryabets <eronryabets@gmail.com>
2022-09-30 18:39:53 +0300

    add DB migration - liquibase (create entity - user, car)

---
+++ src/main/resources/db/changelog/V-1.0/01-add-cars-entity.sql
@@ -0,0 +1,45 @@
+    insert
+    into
+        cars
+        (car_brand, car_number, model, owner_id, year)
+    values
+        ('Subaru', 'AA 1111 AA', 'Forester', '1', 2005)
+
+GO
+
+    insert
+    into
+        cars
+        (car_brand, car_number, model, owner_id, year)
+    values
+        ('Subaru', 'AA 2222 AA', 'Impreza', '2', 2003)
+
+GO
+
+    insert
+    into
+        cars
+        (car_brand, car_number, model, owner_id, year)
+    values
+        ('Honda', 'AA 3333 AA', 'Civic 4D', '1', 2007)
+
+GO
+
+    insert
+    into
+        cars
+        (car_brand, car_number, model, owner_id, year)
+    values
+        ('Honda', 'AA 4444 AA', 'NSX', '3', 2005)
+
+GO
+
+    insert
+    into
+        cars
+        (car_brand, car_number, model,  year)
+    values
+        ('Honda', 'AA 4444 AA', 'S2000',  2007)
+
+GO
+
